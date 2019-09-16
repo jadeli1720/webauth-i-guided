f9a6c879089deaa70553b47baa8fffb36131680a
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-16 11:09:42 -0600

    Validating user password using compareSunc()

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ server.get('/', (req, res) => {
 });
 
 server.post('/api/register', (req, res) => {
-  let user = req.body;
+  let {username, password} = req.body;
+  const hash = bcrypt.hashSync(password, 12);
 
-  Users.add(user)
+  Users.add({username, password: hash})
     .then(saved => {
       res.status(201).json(saved);
     })
@@ -34,7 +35,7 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -56,8 +57,7 @@ server.get('/api/users', (req, res) => {
 server.get('/hash', (req, res) => {
   const name = req.query.name;
 
-  //hash the name
-  const hash = bcrypt.hashSync(name, 12);
+  const hash = bcrypt.hashSync(name, 12);//hash the name using bcrypt
 
   res.send(`the hash for ${name} is ${hash}`)
 })
